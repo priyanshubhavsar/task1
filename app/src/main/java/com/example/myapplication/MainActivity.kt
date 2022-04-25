ffc243b24381e54195f5f80eeda28900f0032271
vp170 <1234>
2022-04-25 12:23:16 +0530

    asnm

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Toast.makeText(this,"${editTextTextPersonName.text.toString().toInt() + editTextTextPersonName2.text.toString().toInt()}",Toast.LENGTH_LONG).show()
+        }
+    }
+}
